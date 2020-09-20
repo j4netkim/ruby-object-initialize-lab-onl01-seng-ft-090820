c9d8fef0f4e0e29c64240c8200ebc6119f96b315
J K <kj688895@gmail.com>
2020-09-20 22:50:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog
   def initialize(name, breed)
-    @name = name 
-    @breed = breed 
-  end 
+    @name = name
+    @breed = breed
+  end
 
   def dog_name=(dog_stat)
-    name, breed = dog_stat.split 
-    @name = name 
-    @breed = breed 
-  end 
+    name, breed = dog_stat.split
+    @name = name
+    @breed = breed
+  end
 
   def dog_name
-    "#{@name}" #{@breed}".strip 
-  end 
+    "#{@name}" #{@breed}".strip
+  end
 end 
