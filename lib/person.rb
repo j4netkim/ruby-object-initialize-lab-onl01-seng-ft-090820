eb5b1f77c118e548f197455ed714d3ca9938f966
J K <kj688895@gmail.com>
2020-09-20 22:45:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
-class Person 
+class Person
   def initialize(name)
-    @name = name 
-  end 
+    @name = name
+  end
 
   def name=(name)
-    @name = name 
-  end 
+    @name = name
+  end
 
-  def name 
-    @name 
-  end 
+  def name
+    @name
+  end
 end 
